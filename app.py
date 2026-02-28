25d4a240dc7efcb442d4647f5dedb14278214a99
Thanakrit-1st <thanafirst1st@gmail.com>
2026-02-28 14:08:00 +0700

    Test

--- app.py
+++ app.py
@@ -4,3 +4,24 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 
+st.sidebar.title("Main Menu")
+page = st.sidebar.selectbox("Select Page", ["Datasets Info", "Machine Learning Model Details","Neural Network Model Details", "Test Machine Learning Model", "Test Neural Network Model"])
+
+if page == "Datasets Info":
+    st.header("TEst")
+    st.write("")
+
+elif page == "Machine Learning Model Details":
+    st.header("")
+    st.write("")
+elif page == "Neural Network Model Details":
+    st.header("")
+    st.write("")
+
+elif page == "Test Machine Learning Model":
+    st.header("")
+    st.write("")
+
+elif page == "Test Neural Network Model":
+    st.header("")
+    st.write("")
